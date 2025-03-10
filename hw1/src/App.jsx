bea8a0551b7d1a5cc6e71a8a2789cfe10065d36e
megraves <megraves@umass.edu>
2025-03-10 15:37:31 -0400

    add TODO comments

--- hw1/src/App.jsx
+++ hw1/src/App.jsx
@@ -3,6 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/** Define Components:
+ * // TODO:
+ * 1. Activity Logging: Create a feature that allows users to log daily activities, categorizing them by type. 
+ *    Each activity will have an associated carbon value, helping users understand their environmental impact.
+ * 2. Goal Setting: Implement a goal-setting feature where users can set personal targets for reducing their carbon footprint,
+ *    with progress tracking and motivational feedback.
+ * 3. Resource Library:  Develop a section with practical tips and resources for sustainable living, 
+ *    encouraging users to adopt eco-friendly habits.
+ * 4. Customizable UI: Allow users to personalize the app’s interface, making it visually appealing and user-friendly.
+ * 5. Summary Views: Design a dashboard that provides a visual summary of the user's carbon footprint over time, 
+ *    using charts or graphs to illustrate trends.
+ */
+
+function SummaryCard() {
+ // welcome!
+ // pie chart of progress
+ // button for resource library
+}
+
+// Used for logging and presenting activities and goals
+function LogCard() {
+  // shows activities for the day
+  // No activities logged yet.
+  // Categorize
+}
+
+function GoalCard() {
+
+}
+
+function ActivityCard() {
+
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
